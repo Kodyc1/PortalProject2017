967c6f55c31f0885e2b9f5de3f2e4f22c24685fd
Kody Cheung <Kody Cheung>
2017-04-16 20:43:09 -0700

    separated logged out person from logged in person

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var current = req.session.user;
   if (current){
-      res.render('index', { title: 'Food Ordering Website', h1: 'Menu', user: req.session.user });
+      res.render('index', { title: 'Food Ordering Website', h1: 'Menu', user: req.session.user, bool: true });
   } else{
     req.session.destroy()
-    res.render('index', { title: 'Food Ordering Website', h1: 'Menu', user: false});
+    res.render('index', { title: 'Food Ordering Website', h1: 'Menu', user: false, bool: false});
   }
 
 
@@ -17,20 +17,20 @@ router.get('/', function(req, res, next) {
 router.get('/carts', function(req, res, next) {
   var current = req.session.user;
   if (current){
-    res.render('cart', { title: 'Cart', h1: 'Cart', user: req.session.user });
+    res.render('cart', { title: 'Cart', h1: 'Cart', user: req.session.user, bool: true });
   } else{
     req.session.destroy()
-    res.render('cart', { title: 'Cart', h1: 'Cart', user:  false });
+    res.render('cart', { title: 'Cart', h1: 'Cart', user: false, bool:  false });
   }
 });
 
 router.get('/login', function(req, res, next) {
   req.session.destroy();
-  res.render('login', { title: 'Login', h1: 'Log In', user: false });
+  res.render('login', { title: 'Login', h1: 'Log In', user: false, bool: false });
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register', h1: 'Sign Up', user: false });
+  res.render('register', { title: 'Register', h1: 'Sign Up', user: false, bool: false });
 });
 
 router.get('/checkout', function(req, res, next) {
